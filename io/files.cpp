24fed0e9a3c43d65ea70a3aa6e22773f824c4019
rodri-r-z <2nw53j32643y@gmail.com>
2024-12-05 20:33:00 -0500

    Rework files logic

--- io/files.cpp
+++ io/files.cpp
@@ -23,10 +23,11 @@
 #include <filesystem>
 #include "../lang/result.hpp"
 #include "../lang/err.hpp"
+#include "../lang/string.hpp"
 
 using namespace std;
 
-Result<bool, GenericErr> write_file(const string* path, const string* content) {
+Result<bool, GenericErr> write_file(const char* path, String content) {
 
     // First check if the file exists
     if (!file_exists(path).unwrap()) {
@@ -35,7 +36,7 @@ Result<bool, GenericErr> write_file(const string* path, const string* content) {
 
     // Open the file
     ofstream file;
-    file.open(*path);
+    file.open(path);
 
     // Something went wrong
     if (!file.is_open()) {
@@ -43,7 +44,7 @@ Result<bool, GenericErr> write_file(const string* path, const string* content) {
     }
 
     // Directly write the content to the file
-    file << content;
+    file << content.to_data();
 
     // Don't forget to close the file!
     file.close();
@@ -52,19 +53,19 @@ Result<bool, GenericErr> write_file(const string* path, const string* content) {
 
 }
 
-Result<string, GenericErr> read_file(const string* path) {
+Result<String, GenericErr> read_file(const char* path) {
 
     // First check if the file exists
     if (!file_exists(path).unwrap()) {
-        return Result(string(""), optional<GenericErr>(GenericErr("File does not exist")));
+        return Result(String(""), optional<GenericErr>(GenericErr("File does not exist")));
     }
 
     // Open the file
     ifstream file;
-    file.open(*path);
+    file.open(path);
 
     if (!file.is_open()) {
-        return Result<string>("", optional<GenericErr>(GenericErr("Failed to open file")));
+        return Result<String>(String(""), optional<GenericErr>(GenericErr("Failed to open file")));
     }
 
     string content;
@@ -78,10 +79,10 @@ Result<string, GenericErr> read_file(const string* path) {
     // Don't forget to close the file!
     file.close();
 
-    return Result<string, GenericErr>(move(content), nullopt);
+    return Result<String, GenericErr>(String(content.c_str()), nullopt);
 }
 
-Result<bool, GenericErr> delete_file(const string* path) {
+Result<bool, GenericErr> delete_file(const char* path) {
     
     // First check if the file exists
     if (!file_exists(path).unwrap()) {
@@ -89,7 +90,7 @@ Result<bool, GenericErr> delete_file(const string* path) {
     }
 
     // Remove the file
-    if (remove(path->c_str()) != 0) {
+    if (remove(path) != 0) {
         return Result(false, optional<GenericErr>(GenericErr("Failed to delete file")));
     }
 
@@ -97,50 +98,50 @@ Result<bool, GenericErr> delete_file(const string* path) {
 
 }
 
-Result<vector<string>, GenericErr> walk_dir(const string* path) {
+Result<Vec<String>, GenericErr> walk_dir(const char* path) {
 
     // First check if the file exists
     if (!file_exists(path).unwrap()) {
-        return Result(vector<string>(), optional<GenericErr>(GenericErr("File does not exist")));
+        return Result(Vec<String>(), optional<GenericErr>(GenericErr("File does not exist")));
     }
 
     // Check if it's a directory
     if (!is_dir(path).unwrap()) {
         return Result(
-            vector<string>(),
+            Vec<String>(),
             optional<GenericErr>(GenericErr("Path is not a directory"))
         );
     }
 
     // Open the directory
-    DIR* dir = opendir(path->c_str());
+    DIR* dir = opendir(path);
 
     if (dir == NULL) {
         return Result(
-            vector<string>(),
+            Vec<String>(),
             optional<GenericErr>(GenericErr("Failed to open directory"))
         );
     }
 
-    vector<string> files;
+    Vec<String> files;
 
     // Read the directory
     struct dirent* entry;
     while ((entry = readdir(dir)) != NULL) {
-        files.push_back(entry->d_name);
+        files.push(entry->d_name);
     }
 
     // Close the directory
     closedir(dir);
 
-    return Result<vector<string>, GenericErr>(move(files), nullopt);
+    return Result<Vec<String>, GenericErr>(move(files), nullopt);
 
 }
 
-Result<bool, GenericErr> file_exists(const string* path) {
+Result<bool, GenericErr> file_exists(const char* path) {
 
     // Check if the file exists
-    if (filesystem::exists(*path)) {
+    if (filesystem::exists(path)) {
         return Result<bool, GenericErr>(true, nullopt);
     }
 
@@ -148,7 +149,7 @@ Result<bool, GenericErr> file_exists(const string* path) {
 
 }
 
-Result<bool, GenericErr> is_dir(const string* path) {
+Result<bool, GenericErr> is_dir(const char* path) {
 
     // First check if the file exists
     if (!file_exists(path).unwrap()) {
@@ -156,26 +157,27 @@ Result<bool, GenericErr> is_dir(const string* path) {
     }
 
     // Check if the path is a directory
-    if (filesystem::is_directory(*path)) {
+    if (filesystem::is_directory(path)) {
         return Result<bool, GenericErr>(true, nullopt);
     }
 
     return Result<bool, GenericErr>(false, nullopt);
 
-}
+}   
 
-Result<bool, GenericErr> remove_dir(const string* path) {
+Result<bool, GenericErr> remove_dir(const char* path) {
 
     // Remember that rmdir from unistd.h only works for empty directories
     // To fix that while still maintaning performance, we can use a queue
     // which has a complexity of O(n) instead of O(n^2) from the recursive approach
 
-    vector<string> queue = { *path };
+    Vec<String> queue = Vec<String>();
+    queue.push(path);
 
-    while (!queue.empty()) {
+    while (!queue.is_empty()) {
         // Get always the first element
-        string* current_path = &queue[0];
-        Result<bool> is_dir_result = is_dir(current_path);
+        String current_path = queue.at(0).unwrap();
+        Result<bool> is_dir_result = is_dir(current_path.to_data());
 
         // Cannot read the directory -> Return the error without crashing
         if (is_dir_result.has_error()) {
@@ -184,35 +186,38 @@ Result<bool, GenericErr> remove_dir(const string* path) {
 
         if (is_dir_result.unwrap()) {
             // See if the directory is empty
-            Result<vector<string>> files = walk_dir(current_path);
+            Result<Vec<String>> files = walk_dir(current_path.to_data());
 
             // Cannot read the directory -> Return the error without crashing
             if (files.has_error()) {
                 return Result<bool, GenericErr>(false, optional<GenericErr>(files.get_error()));
             }
 
-            vector<string> files_vec = files.unwrap();
+            Vec<String> files_vec = files.unwrap();
 
             // If the directory is empty, remove it
-            if (files_vec.size() == 2) {
-                if (rmdir(current_path->c_str()) != 0) {
+            if (files_vec.length() == 2) {
+                if (rmdir(current_path.to_data()) != 0) {
                     return Result<bool, GenericErr>(false, optional<GenericErr>(GenericErr("Failed to delete directory")));
                 }
             } else {
                 // Add all the files to the queue
-                for (const string& file : files_vec) {
-                    if (file != "." && file != "..") {
-                        queue.push_back(*current_path + "/" + file);
+                for (const String& file : files_vec) {
+                    if (file.compare_raw(".") && file.compare_raw("..")) {
+                        queue.push(
+                            current_path.join("/")
+                                .join(file)
+                        );
                     }
                 }
 
                 // Add the directory to the end of the queue
                 // After all files are deleted, the directory should be now empty
-                queue.push_back(*current_path);
+                queue.push(current_path);
             }
         } else {
             // Try to remove the file
-            Result<bool, GenericErr> delete_result = delete_file(current_path);
+            Result<bool, GenericErr> delete_result = delete_file(current_path.to_data());
 
             // Cannot delete the file -> Return the error without crashing
             if (delete_result.has_error()) {
@@ -221,7 +226,7 @@ Result<bool, GenericErr> remove_dir(const string* path) {
         }
 
         // Pop the first element
-        queue.erase(queue.begin());
+        queue.shift(1);
     }
 
     return Result<bool, GenericErr>(true, nullopt);
